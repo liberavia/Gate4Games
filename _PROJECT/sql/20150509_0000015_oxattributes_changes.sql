2da2406baa6f415ff77bbdde4f12087e01802397
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-05-09 14:56:05 +0000

    0000015: Artikelattribute anlegen * Added module to be able to extend attributes
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@37 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- _PROJECT/sql/20150509_0000015_oxattributes_changes.sql
+++ _PROJECT/sql/20150509_0000015_oxattributes_changes.sql
@@ -5,13 +5,13 @@ INSERT INTO `oxattribute` (`OXID`, `OXSHOPID`, `OXTITLE`, `OXTITLE_1`, `OXTITLE_
 ('CompatibilityTypeMac', 'oxbaseshop', 'Kompatibilität MacOSX', 'Compatibility MacOSX', '', '', 5, '2015-05-09 13:35:48', 0, ''),
 ('DRM', 'oxbaseshop', 'Kopierschutz (DRM)', 'Copy Protection (DRM)', '', '', 2, '2015-05-09 13:35:23', 0, ''),
 ('GameGenre', 'oxbaseshop', 'Genre', 'Genre', '', '', 0, '2015-05-09 13:31:45', 0, ''),
-('GameLanguageInterface', 'oxbaseshop', 'Sprache Text', 'Language Interface', '', '', 8, '2015-05-09 13:39:54', 0, ''),
-('GameLanguageSubtitles', 'oxbaseshop', 'Sprache Untertitel', 'Language Subtitles', '', '', 9, '2015-05-09 13:39:58', 0, ''),
-('GameLanguageAudio', 'oxbaseshop', 'Sprache Ton', 'Language Audio', '', '', 7, '2015-05-09 13:39:50', 0, ''),
-('ReleaseDate', 'oxbaseshop', 'Veröffentlichungsdatum', 'Release Date', '', '', 12, '2015-05-09 13:40:13', 0, ''),
+('GameLanguageInterface', 'oxbaseshop', 'Sprache Text', 'Language Interface', '', '', 9, '2015-05-09 14:52:32', 0, ''),
+('GameLanguageSubtitles', 'oxbaseshop', 'Sprache Untertitel', 'Language Subtitles', '', '', 10, '2015-05-09 14:52:36', 0, ''),
+('GameLanguageAudio', 'oxbaseshop', 'Sprache Ton', 'Language Audio', '', '', 8, '2015-05-09 14:52:29', 0, ''),
+('ReleaseDate', 'oxbaseshop', 'Veröffentlichungsdatum', 'Release Date', '', '', 13, '2015-05-09 14:52:46', 0, ''),
 ('CompatibilityTypeLin', 'oxbaseshop', 'Kompatibilität Linux', 'Compatibility Linux', '', '', 4, '2015-05-09 13:35:36', 0, ''),
 ('GameType', 'oxbaseshop', 'Spieltyp', 'Game Type', '', '', 1, '2015-05-09 13:35:11', 0, ''),
-('RecommendedAgeUsk', 'oxbaseshop', 'Altersempfehlung USK', 'Recommended Age USK', '', '', 10, '2015-05-09 13:40:03', 0, ''),
-('RecommendedAgePegi', 'oxbaseshop', 'Altersempfehlung Pegi', 'Recommended Age Pegi', '', '', 11, '2015-05-09 13:40:09', 0, ''),
-('CompatibilityTypeWin', 'oxbaseshop', 'Kompatibilität Windows', 'Compatibility Windows', '', '', 3, '2015-05-09 13:35:30', 0, '');
-
+('RecommendedAgeUsk', 'oxbaseshop', 'Altersempfehlung USK', 'Recommended Age USK', '', '', 11, '2015-05-09 14:52:40', 0, ''),
+('RecommendedAgePegi', 'oxbaseshop', 'Altersempfehlung Pegi', 'Recommended Age Pegi', '', '', 12, '2015-05-09 14:52:43', 0, ''),
+('CompatibilityTypeWin', 'oxbaseshop', 'Kompatibilität Windows', 'Compatibility Windows', '', '', 3, '2015-05-09 13:35:30', 0, ''),
+('CompatibilityTypePOL', 'oxbaseshop', 'Kompatibilität PlayOnLinux', 'Compatibility PlayOnLinux', '', '', 7, '2015-05-09 14:52:26', 0, '');
