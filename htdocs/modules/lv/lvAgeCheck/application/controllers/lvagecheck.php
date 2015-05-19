42fc68296b8c468e3a3e7035f1c2a18cd8e4fab9
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-05-19 23:48:55 +0000

    0000007: Modul: Altersbestätigung * Changed parameter to be an article id so all information can be leeched by oxarticle object
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@135 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- htdocs/modules/lv/lvAgeCheck/application/controllers/lvagecheck.php
+++ htdocs/modules/lv/lvAgeCheck/application/controllers/lvagecheck.php
@@ -244,6 +244,12 @@ class lvagecheck extends oxUBase {
     public function lvValidateAge() {
         $oConfig = $this->getConfig();
         
+        $sArticleId     = $oConfig->getRequestParameter( 'anid' );
+        if ( $sArticleId ) {
+            $this->_oLvArticle = oxNew( 'oxarticle' );
+            $this->_oLvArticle->load( $sArticleId );
+        }
+        
         $aParams    = $oConfig->getRequestParameter( 'editval' );
         $sReturnUrl = $this->lvGetReturnUrl();
         
