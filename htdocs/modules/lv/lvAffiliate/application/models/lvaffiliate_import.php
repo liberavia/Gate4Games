77c04c411b63be6596a2aef927b11c5ebdd21798
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-05-31 20:05:15 +0000

    0000011: Modul: Framework zur Affiliate-Anbindung * Building Affiliate import mechanisms
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@203 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- htdocs/modules/lv/lvAffiliate/application/models/lvaffiliate_import.php
+++ htdocs/modules/lv/lvAffiliate/application/models/lvaffiliate_import.php
@@ -31,20 +31,161 @@ class lvaffiliate_import extends oxBase {
      */
     protected $_sLvVendorId = null;
     
+    /**
+     * ManufacturerId of current importing article
+     * @var string
+     */
+    protected $_sLvCurrentManufacturerId = null;
+
+    /**
+     * Current set of article data needs to be importet
+     * @var array
+     */
+    protected $_aLvCurrentArticleData = null;
+    
+    /**
+     * Configuration for matching manufacturer logic
+     * @var array
+     */
+    protected $_aLvField2MatchManufacturer = null;
+    
+    /**
+     * Configuration for article matching logic
+     * @var array
+     */
+    protected $_aLvField2MatchArticle = null;
+    
+    /**
+     * Configuration for direct assignments from data array to tables and fields
+     * @var array
+     */
+    protected $_aLvField2DirectTable = null;
+    
+    /**
+     * Configuration for assigning to categories
+     * @var array
+     */
+    protected $_aLvField2CategoryAssignment = null;
+    
+    /**
+     * Configuration for assignin attribute values
+     * @var array
+     */
+    protected $_aLvField2Attribute = null;
+
+    /**
+     * Logfile used
+     * @var string
+     */
+    protected $_sLogFile = 'lvaffiliate_import.log';
+
+    
+    
+    public function __construct() {
+        // loading configuration into object
+        $oConfig = $this->getConfig();
+        $this->_aLvField2MatchManufacturer = $oConfig->getConfigParam( 'aLvField2MatchManufacturer' );
+        $this->_aLvField2MatchArticle = $oConfig->getConfigParam( 'aLvField2MatchArticle' );
+        $this->_aLvField2DirectTable = $oConfig->getConfigParam( 'aLvField2DirectTable' );
+        $this->_aLvField2CategoryAssignment = $oConfig->getConfigParam( 'aLvField2CategoryAssignment' );
+        $this->_aLvField2Attribute = $oConfig->getConfigParam( 'aLvField2Attribute' );
+        
+        parent::__construct();
+    }
+    
     
     /**
      * Setter for vendor id
      * 
      * @param string $sVendorId
+     * @return void
      */
     public function lvSetVendorId( $sVendorId ) { 
         $this->_sLvVendorId = $sVendorId;
     }
 
 
-    public function lvAddArticle( $aArticleData ) {
-        echo $this->_sLvVendorId."\n";
+    /**
+     * Loggs a message depending on the defined loglevel. Default is debug-level
+     * 
+     * @param string $sMessage
+     * @param int $iLogLevel
+     * @return void
+     */
+    public function lvLog( $sMessage, $iLogLevel=4 ) {
+        $oUtils = oxRegistry::getUtils();
+        
+        if ( $iLogLevel <= $this->_iLvAmzPnLogLevel ) {
+            $sPrefix        = "[".date( 'Y-m-d H:i:s' )."] ";
+            $sFullMessage   = $sPrefix.$sMessage;
+            
+            $oUtils->writeToLog($sFullMessage, $this->_sLogFile );
+        }
+    }
+
+    
+    /**
+     * Method adds/updates article depending on configuration settings and article data
+     * 
+     * @param array $aArticleData
+     * @return void
+     */
+    public function lvAddArticle( $aArticleData, $s ) {
+        $this->_aLvCurrentArticleData = $aArticleData;
+        $this->_lvSetManufacturerId();
+//        $this->_lvSetArticleIds();
+//        $this->_lvSetArticleData();
+//        $this->_lvAssignCategories();
+//        $this->_lvAssignAttributes();
+        
         print_r( $aArticleData );
+    }
+    
+    
+    /**
+     * Method tries to match an existing manufacturer and sets id.
+     * If manufacturer could not be matched it will be created and its ID will be used
+     * 
+     * @param void
+     * @return void
+     */
+    protected function _lvSetManufacturerId() {
+        $oDb = oxDb::getDb( MODE_FETCH_ASSOC );
+        
+        // go through configuration
+        foreach ( $this->_aLvField2MatchManufacturer as $sDataFieldName=>$sConfigFieldValue ) {
+            $aConfigFieldValue  = explode( "|", $sConfigFieldValue );
+            $sConfigDbTable     = $aDataFieldValue[0];
+            $sConfigDbField     = $aDataFieldValue[1];
+            
+            $sQuery = "SELECT OXID FROM ".$sConfigDbTable." WHERE ".$sConfigDbField."='".$this->_aLvCurrentArticleData[$sDataFieldName]."' LIMIT 1";
+            $sManufacturerId = $oDb->GetOne( $sQuery );
+            
+            if ( !$sManufacturerId ) {
+                $this->_lvCreateManufacturer( $this->_aLvCurrentArticleData[$sDataFieldName] );
+            }
+            else {
+                $this->_sLvCurrentManufacturerId = $sManufacturerId;
+            }
+        }
+    }
+    
+    
+    /**
+     * Creating new manufacturer with given name
+     * 
+     * @param string $sManufacturerName
+     * @return void
+     */
+    protected function _lvCreateManufacturer( $sManufacturerName ) {
+        $oUtilsObject   = oxRegistry::get( 'oxUtilsObject' );
+        $sNewId         = $oUtilsObject->generateUId();
+        $oManufacturer  = oxNew( 'oxmanufacturer' );
+        
+        $oManufacturer->setId( $sNewId );
+        $oManufacturer->oxmanufacturers__oxtitle = new oxField( $sManufacturerName );
+        $oManufacturer->save();
+        $this->_sLvCurrentManufacturerId = $sNewId;
     }
     
 }
