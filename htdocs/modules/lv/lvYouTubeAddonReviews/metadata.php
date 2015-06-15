d65284ad57a573e0437f250a7c5713b034efcaef
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-06-15 23:28:45 +0000

    0000031: Modul: Youtube videos + Erweiterung für Gamestar-Review * Added youtube extensions for reviews and letsplays * Some fixes/adjustments on core code of youtube module
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@261 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- htdocs/modules/lv/lvYouTubeAddonReviews/metadata.php
+++ htdocs/modules/lv/lvYouTubeAddonReviews/metadata.php
@@ -50,7 +50,7 @@ $aModule = array(
     'events'       => array(
     ),
     'templates' => array(
-        'lvyoutube_reviews.tpl'             => 'lv/lvYouTubeAddonReviews/application/views/frontend/page/details/inc/lvyoutube_reviews.tpl'
+        'lvyoutube_reviews.tpl'             => 'lv/lvYouTubeAddonReviews/application/views/frontend/tpl/page/details/inc/lvyoutube_reviews.tpl'
     ),
     'blocks' => array(
         array( 'template' => 'page/details/inc/tabs.tpl',               'block'=>'details_tabs_attributes',                         'file'=>'extend/application/views/blocks/block_widget_details_tabs_attributes.tpl' ),
