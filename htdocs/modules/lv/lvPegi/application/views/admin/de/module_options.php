291d5f7ee2118b5de2ae5a3c120426fbc4fb0e35
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-06-23 18:29:33 +0000

    0000032: Modul: Altersemfehlungen von USK und PEGI Scrapen (Aufsetzen auf bestehendem Modul) * First commit of module * Implemented initial importing a csv file
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@319 22d369d5-c360-4d0a-819e-37d15cbb1d4a

---
+++ htdocs/modules/lv/lvPegi/application/views/admin/de/module_options.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * Copyright (C) 2015 André Gregor-Herrmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+
+$sLangName = 'Deutsch';
+
+$aLang = array(
+    'charset'                                           => 'UTF-8',
+    // groups
+    'SHOP_MODULE_GROUP_lvpegimain'                      => 'Einstellungen',
+    // settings
+    'SHOP_MODULE_sLvPegiRequestBase'                    => 'Basisadresse zur Abfrage.',
+    'SHOP_MODULE_sLvPegiRequestPastMonths'              => 'Anzahl der Monate die laufend abgefragt werden sollen.',
+    'SHOP_MODULE_sLvPegiInitImportFolder'               => 'Ordner in welchem die initiale CSV Datei zu finden ist.',
+    'SHOP_MODULE_sLvPegiInitImportFile'                 => 'Dateiname der initialen CSV-Datei',
+    'SHOP_MODULE_sLvPegiAttributeId'                    => 'Attributs-Id welche die Pegi-Altersinformation enthält',
+);
+
