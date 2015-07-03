aac329ce97272cda5b027c2733ef81bfeeec366d
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2015-07-03 23:46:41 +0000

    0000013: Webseitendesign - Erste Gestaltung * Try to implement sticky menu
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@382 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- htdocs/out/gate4games/src/js/gate4games.js
+++ htdocs/out/gate4games/src/js/gate4games.js
@@ -15,32 +15,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Create a clone of the menu, right next to original.
-//$('#header').addClass('original').clone().insertAfter('#header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-//
-//scrollIntervalID = setInterval(stickIt, 10);
-//
-//
-//function stickIt() {
-//
-//  var orgElementPos = $('.original').offset();
-//  orgElementTop = orgElementPos.top;               
-//
-//  if ($(window).scrollTop() >= (orgElementTop)) {
-//    // scrolled past the original position; now only show the cloned, sticky element.
-//
-//    // Cloned element should always have same left position and width as original element.     
-//    orgElement = $('.original');
-//    coordsOrgElement = orgElement.offset();
-//    leftOrgElement = coordsOrgElement.left;  
-//    widthOrgElement = orgElement.css('width');
-//    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-//    $('.original').css('visibility','hidden');
-//  } else {
-//    // not scrolled past the menu; only show the original menu.
-//    $('.cloned').hide();
-//    $('.original').css('visibility','visible');
-//  }
-//}
+scrollIntervalID = setInterval(lvStickMenu, 10);
+
+
+function lvStickMenu() {
+
+    var targetElementPos = $('#navigation').offset();
+    targetElementTop = targetElementPos.top;               
+
+    if ($(window).scrollTop() >= (targetElementTop)) {
+        // scrolled past the original position; now only show the cloned, sticky element.
+        // make header sticky and hide 
+        $('#header').css( 'top', '-125px' );
+        $('#header').css( 'position', 'fixed' );
+    } 
+    else {
+        // not scrolled past the menu; set css to original
+        $('#header').css( 'top', '0px' );
+        $('#header').css( 'position', 'relative' );
+    }
+}
 
 
