6681366868a522e7f2ccbb73b9936792a3b71e03
liberavia <liberavia@22d369d5-c360-4d0a-819e-37d15cbb1d4a>
2016-03-05 22:50:13 +0000

    Added SteamController support
    git-svn-id: https://svn.gate4games.com/svn/Gate4Games/trunk@651 22d369d5-c360-4d0a-819e-37d15cbb1d4a

--- kodi/addons/script.gateoschrome.launcher/resources/scripts/chrome-launch.sh
+++ kodi/addons/script.gateoschrome.launcher/resources/scripts/chrome-launch.sh
@@ -1,10 +1,14 @@
  #!/bin/bash
 killall -9 kodi.bin
 qjoypad DesktopMode &
+sc-desktop.py stop
+sc-xbox.py start
 # starting the overlay
 # ~/.kodi/addons/script.gateoschrome.launcher/resources/scripts/overlay_trigger.py
 PID_OL_TRIGGER=$!
 /usr/bin/google-chrome-stable -kiosk https://ixquick.com/
+sc-xbox.py stop
+sc-desktop.py start
 killall -9 qjoypad
 kill -9 $PID_OL_TRIGGER
 /usr/bin/kodi -fs
